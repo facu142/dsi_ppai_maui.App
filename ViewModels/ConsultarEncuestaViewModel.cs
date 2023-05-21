13e50861052e39f9e47b586b901a61b9e7e489e8
Facundo Montenegro <90329@sistemas.frc.utn.edu.ar>
2023-05-21 19:30:40 -0300

    agrega mock de llamadas

--- ViewModels/ConsultarEncuestaViewModel.cs
+++ ViewModels/ConsultarEncuestaViewModel.cs
@@ -1,13 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using dsi_ppai_maui.Models;
+using System.Collections.ObjectModel;
 
 namespace dsi_ppai_maui.ViewModels
 {
-    public partial class ConsultarEncuestaViewModel: ObservableObject
+    public partial class ConsultarEncuestaViewModel : ObservableObject
     {
 
         [ObservableProperty]
@@ -16,11 +13,18 @@ namespace dsi_ppai_maui.ViewModels
         [ObservableProperty]
         DateOnly fechaHasta;
 
+        public ObservableCollection<Llamada> Llamadas { get; set; } = new ObservableCollection<Llamada>();
 
         public ConsultarEncuestaViewModel()
         {
             fechaDesde = new DateOnly();
             fechaHasta = new DateOnly();
+
+            Llamada llamada1 = new Llamada { DescripcionOperador = "ASD", Duracion = "20", DetalleAccionRequerida = "Detalle de Accion", EncuestaEnviada = "Encuesta enviada", ObservacionAuditor = "observacion" };
+            Llamada llamada2 = new Llamada { DescripcionOperador = "BDF", Duracion = "10", DetalleAccionRequerida = "Detalle de Accion", EncuestaEnviada = "Encuesta enviada", ObservacionAuditor = "observacion" };
+
+            Llamadas.Add(llamada1);
+            Llamadas.Add(llamada2);
         }
 
     }
