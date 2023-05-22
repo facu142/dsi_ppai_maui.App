9b6f5f91bee1d4c4a11545592f77caeaef02a484
Facundo Montenegro <facundo.montenegro@incluit.com>
2023-05-22 01:30:59 -0300

    detalle llamada con iconos

--- Models/Llamada.cs
+++ Models/Llamada.cs
@@ -13,14 +13,12 @@ namespace dsi_ppai_maui.Models
         public string Duracion { get; set; }
         public string EncuestaEnviada { get; set; }
         public string ObservacionAuditor { get; set; }
-
         public List<RespuestaCliente> RespuestasDeEncuesta { get; set; }
-
         public List<CambioEstado> CambioDeEstado {get; set;}
+        public Cliente Cliente { get; set; }
         public string DeterminarUltimoEstado => CambioDeEstado.LastOrDefault()?.Estado?.Nombre;
-
         public string DeterminarFechaHoraUltimoEstado => CambioDeEstado.LastOrDefault().FechaHoraInicio.ToString();
-
+        public string DeterminarNombreCliente => Cliente.NombreCompleto;
         public Llamada()
         {
             RespuestasDeEncuesta = new List<RespuestaCliente>();
--- ViewModels/ConsultarEncuestaViewModel.cs
+++ ViewModels/ConsultarEncuestaViewModel.cs
@@ -38,15 +38,21 @@ namespace dsi_ppai_maui.ViewModels
 
             cambiosDeEstado2.Add(cambio1);
 
-            Llamada llamada1 = new() { DescripcionOperador = "Desc 1", Duracion = "20", DetalleAccionRequerida = "Detalle de Accion", EncuestaEnviada = "Encuesta enviada", ObservacionAuditor = "observacion Auditor", CambioDeEstado = cambiosDeEstado1 };
-            Llamada llamada2 = new() { DescripcionOperador = "Desc 2", Duracion = "10", DetalleAccionRequerida = "Detalle de Accion", EncuestaEnviada = "Encuesta enviada", ObservacionAuditor = "observacion Auditor", CambioDeEstado = cambiosDeEstado2 };
+            Cliente cliente1 = new() { NombreCompleto = "John Doe", Dni = "33999999", nroCelular = "351678911" };
+            Cliente cliente2 = new() { NombreCompleto = "Jane Smith", Dni = "44888888", nroCelular = "351234567" };
+            Cliente cliente3 = new() { NombreCompleto = "Bob Johnson", Dni = "55666666", nroCelular = "351987654" };
+            Cliente cliente4 = new() { NombreCompleto = "Alice Williams", Dni = "66777777", nroCelular = "351111111" };
+            Cliente cliente5 = new() { NombreCompleto = "Mike Davis", Dni = "77444444", nroCelular = "351222222" };
+
+            Llamada llamada1 = new() { DescripcionOperador = "Desc 1", Duracion = "20", DetalleAccionRequerida = "Detalle de Accion", EncuestaEnviada = "Encuesta enviada", ObservacionAuditor = "observacion Auditor", CambioDeEstado = cambiosDeEstado1, Cliente=cliente1};
+            Llamada llamada2 = new() { DescripcionOperador = "Desc 2", Duracion = "10", DetalleAccionRequerida = "Detalle de Accion", EncuestaEnviada = "Encuesta enviada", ObservacionAuditor = "observacion Auditor", CambioDeEstado = cambiosDeEstado2, Cliente = cliente2 };
 
             Llamadas.Add(llamada1);
             Llamadas.Add(llamada2);
         }
 
         [RelayCommand]
-        public async void GoToDetail( Llamada llamada )
+        public async void GoToDetail(Llamada llamada)
         {
             var navParam = new Dictionary<string, object>();
             navParam.Add("DetalleLlamada", llamada);
