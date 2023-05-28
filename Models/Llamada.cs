762d4f5f6022582b89ee19e86a36b01356f5986e
Facundo Montenegro <90329@sistemas.frc.utn.edu.ar>
2023-05-28 12:21:34 -0300

    borre importaciones innecesarias

--- Models/Llamada.cs
+++ Models/Llamada.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.ApplicationModel.Activation;
-using Windows.Media.Audio;
+
 
 namespace dsi_ppai_maui.Models
 {
